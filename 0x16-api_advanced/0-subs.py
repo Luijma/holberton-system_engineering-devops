3476ca01fef0de04a3352aeb33c6d33beac9729e
Luijma <rsluismanuel@gmail.com>
2022-05-31 23:37:49 +0000

    fixed typo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json"format(subreddit)
-    header = {"User-Agent": "User Agent"}
+    headers = {"User-Agent": "User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code = 200:
