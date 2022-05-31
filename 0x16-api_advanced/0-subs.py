e348fd8a1832029d795fd11f726b6bcf87f2ef52
Luijma <rsluismanuel@gmail.com>
2022-05-31 23:52:25 +0000

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code = 200:
+    if response.status_code == 200:
         return response.json().get("data").get("subscribers")
     else:
         return 0
