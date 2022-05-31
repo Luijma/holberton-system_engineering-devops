d5315676dca6ec8cb00caac7f7177e4f454b52d8
Luijma <rsluismanuel@gmail.com>
2022-05-31 23:44:48 +0000

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ Returns number of subscriber sin subreddit """
+
     url = "https://www.reddit.com/r/{}/about.json"format(subreddit)
     headers = {"User-Agent": "User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
